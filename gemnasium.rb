3baf32688d4e1be6ca2e1451a3efda9dc5e44d6e
Khash Sajadi <khash@sajadi.co.uk>
2013-12-02 16:45:14 +0000

    fixed API call issue

--- gemnasium.rb
+++ gemnasium.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Gemnasium
   
@@ -7,7 +8,7 @@ class Gemnasium
     auth = {:username => "X", :password => ENV['GEMNASIUM_API_KEY'] }
     dependencies = [ { :name => 'Gemfile', :content => gemfile }, { :name => 'Gemfile.lock', :content => gemfile_lock }]
 
-    load = { :body => { :dependency_files => dependencies }, :basic_auth => auth }
+    load = { :body => { :dependency_files => dependencies }.to_json, :basic_auth => auth }
     load.merge!({ :timeout => timeout }) if timeout != 0
   
     begin
@@ -16,8 +17,11 @@ class Gemnasium
       if response.code != 200
         raise "Received #{response.code} from Gemnasium: #{response.message}"
       else
-        return { :status => response }
+        return { :status => response.parsed_response['status'] }
       end
+    rescue Timeout::Error
+      puts "Gemnasium API call timedout" 
+      return { :status => :unknown }
     rescue => exc
       puts "Gemnasium API call failed #{exc} at #{exc.backtrace.join("\n")}" 
       return { :status => :unknown }
